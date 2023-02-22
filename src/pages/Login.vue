6b8fb9238ed49b818fcf12a875dc06c19ff3b639
JinsupJung <cool4uh@gmail.com>
2023-02-22 23:19:35 +0900

    01:00:47 7. login하고 백엔드에서 데이터 받음

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="form-signin w-100 m-auto">
-    <form>
       <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
       <div class="form-floating">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <input type="email" v-model="state.form.email" class="form-control" id="floatingInput" placeholder="name@example.com">
         <label for="floatingInput">Email address</label>
       </div>
       <div class="form-floating">
-        <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+        <input type="password" v-model="state.form.password" class="form-control" id="floatingPassword" placeholder="Password">
         <label for="floatingPassword">Password</label>
       </div>
 
@@ -17,15 +15,33 @@
           <input type="checkbox" value="remember-me"> Remember me
         </label>
       </div>
-      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
-    </form>
+<!--      <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>-->
+      <button class="w-100 btn btn-lg btn-primary" @click="submit()">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2023</p>
   </div>
 </template>
 
 <script>
+import {reactive} from "vue";
+import axios from "axios";
+
 export default {
-  name: "Login"
+  name: "Login",
+  setup() {
+    const state=reactive({
+      form:{ email:"", password:"" }
+      })
+
+    const submit=()=>{
+      axios.post("http://localhost:8080/api/account/login", state.form)
+          .then(res=>{
+            console.log(res);
+            window.alert("로그인했습니다.")
+          })
+    }
+
+    return {state, submit};
+  }
 }
 </script>
 
