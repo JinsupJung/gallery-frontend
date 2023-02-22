10ab9118a6ff7a4885fc586d55fccab5838a5f16
JinsupJung <cool4uh@gmail.com>
2023-02-22 18:28:49 +0900

    18:12 3. Back=>Image 가져오기까지

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,9 +3,10 @@
     <div class="album py-5 bg-light">
       <div class="container">
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
           <div class="col" v-for="(item, idx) in state.items" :key="idx">
-            {{item}}
             <Card :item="item"></Card>
+<!--            <Card></Card>-->
 
           </div>
         </div>
